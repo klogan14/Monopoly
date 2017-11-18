166594e66dd009b193c5291d15d94ab2657134f6
klogan14 <klogan14@winona.edu>
2017-11-18 12:07:11 -0600

    Added automate import, save csv, data.csv files

--- Monopoly/src/monopoly/Monopoly.java
+++ Monopoly/src/monopoly/Monopoly.java
@@ -5,16 +5,26 @@
  */
 package monopoly;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  *
  * @author John
  */
 public class Monopoly {
 
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException 
+    {
+        SaveCSV app = new SaveCSV();
+        app.saveGameCsv("P1", "Car", 0);
+        File csvFie = new File("/Users/be0754kc/Documents/CS471/MonopolyDB.csv");
+        
+        System.out.println();
         // TODO code application logic here
     }
     
