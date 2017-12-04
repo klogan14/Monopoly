cc03eedcf66f334ec2dbdb2b664588964156edc8
klogan14 <klogan14@winona.edu>
2017-12-03 22:52:30 -0600

    Fixed db load problem as well

--- Monopoly/src/monopoly/MonopolyGame.java
+++ Monopoly/src/monopoly/MonopolyGame.java
@@ -33,17 +33,35 @@ public class MonopolyGame {
         List gameInfo = new ArrayList();
         gameInfo = game.loadGame("game1");
         Player p;
+        players.remove(0);
+//        p = new Player(gameInfo.get(0).toString(),dice, board);
+        p = new Player(gameInfo.get(3).toString(),dice, board);
+        
+        int locIndex = Integer.parseInt(gameInfo.get(4).toString());
+        int turnIndex = Integer.parseInt(gameInfo.get(5).toString());
+
+//        int locIndex = Integer.parseInt(gameInfo.get(1).toString());
+//        int turnIndex = Integer.parseInt(gameInfo.get(2).toString());
+        p.setLocation(locIndex);
+        p.setTurn(turnIndex);
+        System.out.println("player "+p.getName() +" turn " + p.getPlayerTurn() );
+
+        players.add(p);
+        
+        
+        players.remove(0);
         p = new Player(gameInfo.get(0).toString(),dice, board);
-        int locIndex = Integer.parseInt(gameInfo.get(1).toString());
-        int turnIndex = Integer.parseInt(gameInfo.get(2).toString());
+        locIndex = Integer.parseInt(gameInfo.get(1).toString());
+        turnIndex = Integer.parseInt(gameInfo.get(2).toString());
+       //p = new Player(gameInfo.get(3).toString(),dice, board);
+       // locIndex = Integer.parseInt(gameInfo.get(4).toString());
+       // turnIndex = Integer.parseInt(gameInfo.get(5).toString());
         p.setLocation(locIndex);
-        p.setTurn(locIndex);
-        p = new Player(gameInfo.get(4).toString(),dice, board);
-        locIndex = Integer.parseInt(gameInfo.get(5).toString());
-        turnIndex = Integer.parseInt(gameInfo.get(6).toString());
-        p.setLocation(locIndex);
-        p.setTurn(locIndex);
-        roundsPlayed = Integer.parseInt(gameInfo.get(7).toString());
+        p.setTurn(turnIndex);
+        System.out.println("player "+p.getName() +" turn " + p.getPlayerTurn() );
+        players.add(p);
+
+        roundsPlayed = Integer.parseInt(gameInfo.get(6).toString());
     }
     
     public void loadingGameCSV()
